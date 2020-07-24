3b0285bdf5c32b6da8cc0b57e4f515e9897114cf
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-24 21:02:23 +0800

    add

--- Урок №6 - Формат JSON/index.php
+++ Урок №6 - Формат JSON/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Формат JSON</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
+
 <body>
 
     <!-- Формат JSON позваляет передавать массив на сервер
@@ -15,33 +17,37 @@
     -->
 
     <label>Pass country:</label>
-    <select name="country" id="">
+    <select name="country">
         <option value="0" selected="selected"></option>
         <option value="1">USA</option>
         <option value="2">France</option>
     </select>
     <label>States:</label>
-    <select name="city" id=""></select>
-        <option value="0"></option>
+    <select name="city"></select>
+    <option value="0"></option>
     <script>
-
-        $(document).ready(function(){
+        $(document).ready(function() {
             // выборка элемента, change - изменение значения
-            $('select[name="country"]').bind('change', function(){
+            $('select[name="country"]').bind('change', function() {
+                // Очистить что бы не добавлялось
+                $('select[name="city"]').empty();
                 // в тех ajax есть несколько способов передавать данные
                 // раньше писали ajax , сейчас напишем get можно post 
                 // куда отправляем , что отправлем , что получаем
-                $.get("countryCheck.php", {country: $('select[name="country"]').val()}, function(data){
+                $.get("countryCheck.php", {
+                    country: $('select[name="country"]').val()
+                }, function(data) {
                     // получаем данные и выводим их на экран
                     // здесь мы получаем фомат json c бека и должны раскодировать
                     // JSON.parse для преобразования JSON обратно в объект.
                     data = JSON.parse(data);
-
-
+                    for (var key in data) {
+                        $('select[name="city"]').append($("<option value='" + key + "'>" + data[key] + "</option>"));
+                    }
                 })
             });
         });
-
     </script>
 </body>
+
 </html>
